b774ac9ae6f6556b3f1968f90b5109470b97ba6f
gregueiras <gregueiras@gmail.com>
2019-03-09 14:47:30 +0000

    roll_up Forwards operator implemented

--- operators.cpp
+++ operators.cpp
@@ -2,6 +2,7 @@
 
 #include "operators.h"
 #include <stdexcept>
+#include <algorithm>
 
 typedef int(*cost_function)(const node& a);
 typedef node(*operators)(node a, cost_function foo);
@@ -19,30 +20,60 @@ node roll_up(node state, cost_function cost)
     const auto y2 = pos.at(1).y;
 
     const auto sideways = y1 == y2; // "Horizontal"
+    const auto forewards = x1 == x2; // "Vertical"
+    const auto min_y = std::min(y1, y2);
 
     if (sideways && y1 > 0)
     {
-       const auto index1 = get_index(x1, y1, state.cols);
-       const auto index2 = get_index(x2, y2, state.cols);
+      // Calculate new position
+      const auto new_x1 = x1;
+      const auto new_y1 = y1 - 1;
+      const auto new_x2 = x2;
+      const auto new_y2 = y2 - 1;
 
-       const auto new_x1 = x1;
-       const auto new_y1 = y1 - 1;
-       const auto new_x2 = x2;
-       const auto new_y2 = y2 - 1;
+      // Update map
+      const auto index1 = get_index(x1, y1, state.cols);
+      const auto index2 = get_index(x2, y2, state.cols);
 
-       const auto new_index1 = get_index(new_x1, new_y1, state.cols);
-       const auto new_index2 = get_index(new_x2, new_y2, state.cols);
+      const auto new_index1 = get_index(new_x1, new_y1, state.cols);
+      const auto new_index2 = get_index(new_x2, new_y2, state.cols);
 
-       std::swap(state.map[index1], state.map[new_index1]);
-       std::swap(state.map[index2], state.map[new_index2]);
+      std::swap(state.map[index1], state.map[new_index1]);
+      std::swap(state.map[index2], state.map[new_index2]);
 
-       state.pos.at(0) = point(new_x1, new_y1);
-       state.pos.at(1) = point(new_x2, new_y2);
+      // Update position
+      state.pos.at(0) = point(new_x1, new_y1);
+      state.pos.at(1) = point(new_x2, new_y2);
 
-       state.cost = cost(state);
+      // Update cost
+      state.cost = cost(state);
 
-       return state;
-       
+      return state;
+
+    }
+    else if (forewards && min_y > 0) {
+
+      // Calculate new position
+      const auto new_x = x1;
+      const auto new_y = min_y - 1;
+
+      // Update Map
+      const auto index1 = get_index(x1, y1, state.cols);
+      const auto index2 = get_index(x2, y2, state.cols);
+
+      const auto new_index = get_index(new_x, new_y, state.cols);
+
+      state.map[index1] = empty_tile;  // TODO: Restore original tile+
+      state.map[index2] = empty_tile;
+      state.map[new_index] = block_tile;
+
+      // Update position
+      state.pos = { point(new_x, new_y) };
+
+      // Update cost
+      state.cost = cost(state);
+
+      return state;
     }
   }
   else if (pos.size() == 1)
