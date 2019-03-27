96b5fa408ce5ef70132e7c09207ffb9584079275
Gonçalo Santos <gregueiras@gmail.com>
2019-03-27 12:04:44 +0000

    main function

--- Bloxorz.cpp
+++ Bloxorz.cpp
@@ -9,20 +9,14 @@
 #include "psapi.h"
 #include "runner.h"
 #include "reader.h"
-
 #define WIN32_LEAN_AND_MEAN
 
-int main() {
+void run(int n, mode mode, heuristic heuristic, std::string file, std::ostream & os) {
+  auto lvl = readToLevel(file);
+
   auto r =
-      //runner(a_star, manhattan_teletransport_distance, LVL1, LVL1_R, LVL1_C);
-	  
-
-
-      runner(dfs, LVL1, LVL1_R, LVL1_C);
+    runner(mode, heuristic, lvl.level_map, lvl.num_rows, lvl.num_columns);
   
-	  //Level lvl = readToLevel("Level1.txt");
-	  //runner(dfs, lvl.level_map, lvl.num_rows, lvl.num_columns);
-
   PROCESS_MEMORY_COUNTERS pmc;
   const auto myHandle = GetCurrentProcess();
   GetProcessMemoryInfo(myHandle, &pmc, sizeof(pmc));
@@ -37,9 +31,14 @@ int main() {
       res_old = res;
       res = (pmc.WorkingSetSize);
     }
-    std::cout << "TIME*50: " << sol << "\tNODES: " << r.getNodes_Analyzed()
-              << "\tMEMORY EXPECTED: " << r.getNodes_Analyzed()*sizeof(node) / 1024.0 << " kB" 
-              << "\tMEMORY MEASURED: " << (res - res_old) / 1024.0 << " kB"
-              << std::endl;
+    os << "TIME*50: " << sol << "\tNODES: " << r.getNodes_Analyzed()
+      << "\tMEMORY EXPECTED: " << r.getNodes_Analyzed()*sizeof(node) / 1024.0 << " kB" 
+      << "\tMEMORY MEASURED: " << (res - res_old) / 1024.0 << " kB"
+      << std::endl;
   }
+}
+
+int main()
+{
+  run(5, a_star, manhattan_teletransport_distance, "./levels/level1.txt", std::cout);
 }
