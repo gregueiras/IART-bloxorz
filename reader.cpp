a77aabbbd36bbf186578f0bcbb53e91a9eac8e84
Afonso <afonsojoseazevedo@gmail.com>
2019-03-27 17:42:47 +0000

    Added error message for inconsistent level sizes

--- reader.cpp
+++ reader.cpp
@@ -10,9 +10,11 @@ Level readToLevel(string filePath) {
 
 	input >> retLevel.num_rows >> comma >> retLevel.num_columns;
 	while (input >> element) {
-		
 		retLevel.level_map.push_back(stoi(element));
 	}
+
+	if (retLevel.level_map.size() != retLevel.num_rows * retLevel.num_columns)
+		cout << "Level loaded has inconsistent values.\n";
 
 	return retLevel;
 }
